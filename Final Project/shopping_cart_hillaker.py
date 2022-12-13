c5b549b347c958940ef2c3fb419bfa92ee4c867b
Isaac Hillaker <izacistan@protonmail.com>
2022-12-13 02:45:09 -0600

    Added better input validation. Trying to figure out how to get a list to print as labels within the shopping cart.

--- Final Project/shopping_cart_hillaker.py
+++ Final Project/shopping_cart_hillaker.py
@@ -9,6 +9,7 @@ FUNCTIONS
 
 
 def invalid_name_input_error():
+    global error_label
     """Displays error message to GUI when a user enters invalid data in the first or last name field."""
     error_label = tkr.Label(root, fg="red", bg="#FFFFFF",
                             text="Name can only contain letters and '-'. Please try again.")
@@ -34,10 +35,21 @@ def add_to_inventory(new_item):
     inventory.append(new_item)
 
 
+# def add_item_to_shopping_cart(customer, item):
+
+def delete_entries():
+    """Deletes the entry text within the first_name_field and last_name_field of the main GUI.
+    This function runs when the create_customer() function is called via the create_customer_button in the main GUI.
+    At the end of that function, delete_entries() runs and the entries are wiped clean for reuse."""
+    first_name_field.delete(0, 35)
+    last_name_field.delete(0, 35)
+    error_label.destroy()
+
+
 def create_customer():
     """This function takes user input from the tkinter GUI fields, validates the input,
     and creates a new customer using the customer class. It also appends the newly created
-    customer to the 'customer_list'."""
+    customer to the 'customer_list' list."""
     name_characters = set(
         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-")  # Characters users may enter in both the first and last name fields.
     customer_id_characters = set(
@@ -45,19 +57,51 @@ def create_customer():
     first_name = first_name_field.get()  # Gets user input from tkinter GUI, first name field.
     last_name = last_name_field.get()  # Gets user input from tkinter GUI, last name field.
     customer_ID = str(r.randint(1000000, 9999999))  # Creates a random, 7-digit customer ID between 1000000 and 9999999.
-    # Validate user input against 'name_characters' and 'customer_id_characters'. Returns and error if given user data is bad.
-    if not (name_characters.issuperset(last_name) and name_characters.issuperset(first_name)):
+
+    if len(first_name.strip()) == 0 or len(
+            last_name.strip()) == 0:  # Makes sure users can't enter NO data or BLANK data.
         return invalid_name_input_error()
+
+    if not (name_characters.issuperset(last_name) and name_characters.issuperset(
+            first_name)):  # Validate user input against 'name_characters' and 'customer_id_characters'. Returns and error if given user data is bad.
+        return invalid_name_input_error()
+
     if not customer_id_characters.issuperset(customer_ID):
         return invalid_customer_id_input_error()
+
     success_msg_label = tkr.Label(root, fg="green", bg="#FFFFFF", text="New customer created!")
-    success_msg_label.grid(row=7, columns=2)
+    success_msg_label.grid(row=7, columns=3)
     customer = Customer(first_name, last_name, customer_ID)
     add_to_customer_list(customer)
+    delete_entries()
     print(customer_list)
 
 
-# def display_shopping_cart():
+def display_shopping_cart():
+    shop_cart_win = tkr.Toplevel()
+    shop_cart_win.geometry('700x700')
+    shop_cart_win.group(root)
+    shop_cart_win.title("Your Cart")
+    shop_cart_win.iconbitmap(
+        'C:/Users/hilla/PycharmProjects/PythonDMACC/Final Project/ecommerce_supermarket_cart_store_grocery_icon_229138.ico')
+    # Exit Button, closes Shopping Cart Window.
+    exit_btn = tkr.Button(shop_cart_win, text="Exit Shopping Cart", padx=35, pady=20, fg="white", bg="red",
+                          command=shop_cart_win.destroy)
+    exit_btn.grid(row=0, column=0)
+
+    # Display Items
+    for rows in range(1, len(inventory) + 1):
+        for c in inventory:
+            tkr.Button(shop_cart_win, text=(getattr(c, '_item_name')), borderwidth=1, padx=btn_width, pady=btn_height).grid(row=rows, column=1)
+    # for rows in range(len(inventory)):
+    #     for item in inventory:
+    #         tkr.Label(shop_cart_win, text=(getattr(item, '_item_name'))).grid(row=rows, column=1)
+
+    item = tkr.Label(shop_cart_win, text=(getattr(item1, '_item_name')))
+    # item.grid(row=1, column=1)
+
+# def add_to_shopping_cart():
+
 
 '''
 *******
@@ -67,7 +111,7 @@ CLASSES
 
 
 class Customer:
-    def __init__(self, first_name, last_name, customer_ID, shopping_cart={}):
+    def __init__(self, first_name, last_name, customer_ID, shopping_cart=[]):
         self._first_name = first_name
         self._last_name = last_name
         self._customer_number = customer_ID
@@ -76,19 +120,22 @@ class Customer:
     def __repr__(self):
         return f"{self._first_name} {self._last_name} {self._customer_number}"
 
-    def display_shopping_cart(self):
-        return self._shopping_cart
+    def add_to_shopping_cart(self, item):
+        self._shopping_cart.append(item)
 
 
 class Item:
-    def __init__(self, item_name, item_number, price, quantity):
+    def __init__(self, item_name, item_number, price):
         self._item_name = item_name
         self._item_number = item_number
         self._price = price
-        self._quantity = quantity
 
     def __repr__(self):
-        return f"{self._item_name} (Item#: {self._item_number})"
+        return f"{self._item_name} (Price: ${self._price})"
+
+    def __str__(self):
+        return f"{self._item_name} (Price: ${self._price})"
+
 
 '''
 ***************************
@@ -97,15 +144,15 @@ INVENTORY and CUSTOMER LIST
 '''
 customer_list = []
 inventory = []
-
+customer1 = Customer("Isaac", "Hillaker", 1004)
 '''
 **************************
 CREATE ITEMS FOR INVENTORY
 **************************
 '''
-item1 = Item("Nintendo Switch", 1001, 299.00, 6)
-item2 = Item("Playstation 5", 1002, 499.00, 0)
-item3 = Item("Xbox Series X", 1003, 499.00, 1)
+item1 = Item("Nintendo Switch", 1001, 299.00)
+item2 = Item("Playstation 5", 1002, 499.00)
+item3 = Item("Xbox Series X", 1003, 499.00)
 add_to_inventory(item1)
 add_to_inventory(item2)
 add_to_inventory(item3)
@@ -133,7 +180,10 @@ exit_button = tkr.Button(root, text="Exit", padx=35, pady=20, fg="white", bg="re
 exit_button.grid(row=0, column=0)
 # Customer Buttons
 create_customer_btn = tkr.Button(root, text="Create New Customer", padx=35, pady=20, command=create_customer)
-create_customer_btn.grid(row=0, column=5)
+create_customer_btn.grid(row=0, column=1)
+# View Shopping Cart Button
+exit_button = tkr.Button(root, text="View Shopping Cart", padx=20, pady=20, command=display_shopping_cart)
+exit_button.grid(row=0, column=2)
 # Create Customer Form
 first_name_label = tkr.Label(root, bg="#FFFFFF", text="First name")  # First name label
 first_name_label.grid(row=1, column=1)
@@ -143,18 +193,12 @@ last_name_label = tkr.Label(root, bg="#FFFFFF", text="Last name")  # Last name l
 last_name_label.grid(row=3, column=1)
 last_name_field = tkr.Entry(root, bg="lightgray", width=btn_width)  # Last name field
 last_name_field.grid(row=4, column=1)
-
-# View Shopping Cart Button
-exit_button = tkr.Button(root, text="View Shopping Cart", padx=20, pady=20)
-exit_button.grid(row=0, column=6)
-
 # Items Gallery
-# item_1 = tkr.Button(root, text="Nintendo Switch", padx=btn_width, pady=btn_height)
-# item_1.pack()
-# item_2 = tkr.Button(root, text="Playstation 5", padx=btn_width, pady=btn_height)
-# item_2.pack()
-# item_3 = tkr.Button(root, text="Xbox Series X", padx=btn_width, pady=btn_height)
-# item_3.pack()
-
+item_1 = tkr.Button(root, text="Nintendo Switch", padx=btn_width, pady=btn_height)
+item_1.grid(row=8, column=0)
+item_2 = tkr.Button(root, text="Playstation 5", padx=btn_width, pady=btn_height)
+item_2.grid(row=8, column=1)
+item_3 = tkr.Button(root, text="Xbox Series X", padx=btn_width, pady=btn_height)
+item_3.grid(row=8, column=2)
 
 root.mainloop()
