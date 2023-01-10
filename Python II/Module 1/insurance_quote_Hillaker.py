4149513b9e4b08838e66c92c5e17fb01ef4182cd
Isaac Hillaker <izacistan@protonmail.com>
2023-01-09 20:12:44 -0600

    insurance_quote_Hillaker.py data validation complete. Customer also successfully added to customer_dict dictionary.

--- Python II/Module 1/insurance_quote_Hillaker.py
+++ Python II/Module 1/insurance_quote_Hillaker.py
@@ -1,10 +1,23 @@
-# 1) Gather User Input
-# Age (integer)
-# Name (string)
-# Coverage Level (string (SM, L, F))
+"""
+    AUTHOR: Isaac Hillaker
+    DATE: 01/09/2023
+    This program takes in user data and gives back a quote for auto insurance based on that data.
+    .
+"""
+# Declare lists, dictionaries, and variables.
+customer_dict = {}
+coverage_options = ["SM", "L", "F"]
+coverage_prices = {"SM": [2593, 608, 552, 525, 494, 515],
+                   "L": [2957, 691, 627, 596, 560, 585],
+                   "F": [6930, 1745, 1564, 1469, 1363, 1402]}
+
+
+# DEFINE FUNCTIONS
 def create_customer():
-    """This function takes user input, validates the input,
-    and adds the customer to the customer_dict dictionary."""
+    """
+    This function takes user input, validates the input,
+    and adds the customer to the customer_dict dictionary.
+    """
 
     global last_name, first_name, age
 
@@ -13,40 +26,49 @@ def create_customer():
         error_msg = "Name can only contains letters and ' - '. Please try again."
         print(error_msg)
 
+    # 1) Gather User Input
     # 2) Try/Except User Input Validation
-    # Gets FIRST and LAST names from user. Validates data.
     name_characters = set(
         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-")  # Characters users may enter in both the first and last name fields.
-    # Validate name input
-    while True:
+
+    while True:  # get FIRST and LAST name and VALIDATE info.
+        first_name = input("Type your first name: ")
+        last_name = input("Type your last name: ")
+        if len(first_name.strip()) == 0 or len(
+                last_name.strip()) == 0:  # Makes sure users can't enter NO data or BLANK data.
+            invalid_name_input_error()
+        elif not (name_characters.issuperset(last_name) and name_characters.issuperset(
+                first_name)):  # Validate user input against 'name_characters'. Returns and error if given user data is bad.
+            invalid_name_input_error()
+        else:
+            break
+
+    while True:  # get AGE and VALIDATE that is an integer and that it is 16 or higher.
         try:
             while True:
-                first_name = input("Type your first name: ")
-                last_name = input("Type your lastname: ")
                 age = int(input("Enter your age. You must be at least 16 years old: "))
-
-                if len(first_name.strip()) == 0 or len(
-                        last_name.strip()) == 0:  # Makes sure users can't enter NO data or BLANK data.
-                    invalid_name_input_error()
-                elif not (name_characters.issuperset(last_name) and name_characters.issuperset(
-                        first_name)):  # Validate user input against 'name_characters'. Returns and error if given user data is bad.
-                    invalid_name_input_error()
-                elif age < 16: # Make sure user is at least 16 years old.
-                    print("You must be at least 16 years old to get a quote.")
+                if age < 16:
+                    print("Must be 16 or older.")
                 else:
-                    break # Leave the inner loop of the try/except statements.
+                    break
         except ValueError:
-            print("You entered bad data. Please try again.") # This is triggered by the age variable containing letters instead of numbers.
+            print("Invalid data. Please try again, entering only numbers.")
             continue
         else:
-            break # Leave the main loop of the try/except statements.
+            break
 
+    while True:  # get coverage type from user and validate.
+        desired_coverage = input("Type your preferred coverage level (SM, L, or F): ")
+        desired_coverage = desired_coverage.upper()  # Converts desired_coverage variable to caps, accounting for user entering in lowercase options.
+        if desired_coverage not in coverage_options:
+            print("Data not valid. Please try again entering SM, L, or F.")
+        else:
+            break  # Leave the inner loop of the try/except statements.
 
-    return first_name, last_name, age
+    # Add new customer data to customer dictionary.
+    customer_dict.update({"001": [first_name, last_name, age, desired_coverage]})
+    return customer_dict  # Last line of create_customer() function
 
-
-
-# 3) Store the above information in a dictionary (you could have an ID be the key and the value be a list of attributes [drivername,driverage,coverage])
 
 # 4) Add the customer’s coverage cost to the dictionary value according to their age and coverage desired using the table above
 
@@ -59,4 +81,5 @@ def create_customer():
 # 7): Ensure that you have docstring at the top of your program with your name/date/program information
 
 # DRIVER CODE
-print(create_customer())
+create_customer()
+print(customer_dict)
